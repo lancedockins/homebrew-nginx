476b12a635a20d7422450183856ced092196a982
Lance Dockins <lance@wordkeeper.com>
2022-05-06 15:46:29 -0500

    Modernize Brotli Formula

--- Formula/brotli-nginx-module.rb
+++ Formula/brotli-nginx-module.rb
@@ -1,9 +1,9 @@
 class BrotliNginxModule < Formula
   desc "NGINX module for Brotli compression"
-  homepage "https://github.com/eustas/ngx_brotli"
-  url "https://github.com/eustas/ngx_brotli/archive/v0.1.2.tar.gz"
-  sha256 "309af9e96c10e80f1884acea96379980979581adc287ce338f084607bd48c185"
-  head "https://github.com/eustas/ngx_brotli.git", branch: "master"
+  homepage "https://github.com/google/ngx_brotli"
+  url "https://github.com/google/ngx_brotli.git", :using => :git
+  head "https://github.com/google/ngx_brotli.git", branch: "master"
+  version "0.1.3"
 
   def install
     pkgshare.install Dir["*"]
