15266f8af85d21564dae9b409d048961d4cd74a0
Lance Dockins <lance@wordkeeper.com>
2022-08-19 19:37:53 -0500

    Bump Headers More Version

--- Formula/headers-more-nginx-module.rb
+++ Formula/headers-more-nginx-module.rb
@@ -1,7 +1,7 @@
 class HeadersMoreNginxModule < Formula
   desc "Set, add, and clear arbitrary output headers"
   homepage "https://github.com/openresty/headers-more-nginx-module"
-  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.33.tar.gz"
+  url "https://github.com/openresty/headers-more-nginx-module/archive/v0.34.tar.gz"
   sha256 "a3dcbab117a9c103bc1ea5200fc00a7b7d2af97ff7fd525f16f8ac2632e30fbf"
 
   def install
