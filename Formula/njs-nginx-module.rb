63d8c29b5ebeb0e64c27877529b8b0869b68560f
Lance Dockins <lance@wordkeeper.com>
2022-12-24 15:33:24 -0600

    Bump NJS Hash

--- Formula/njs-nginx-module.rb
+++ Formula/njs-nginx-module.rb
@@ -2,7 +2,7 @@ class NjsNginxModule < Formula
   desc "Adds support for njs scripting to nginx"
   homepage "https://github.com/nginx/njs"
   url "https://github.com/nginx/njs/archive/0.7.9.tar.gz"
-  sha256 "f9e5b6ee79711ef2fcea6482144c0c901b99ba6bd52d72c1c0ae7415a42f4f45"
+  sha256 "92cc425d0b0952bb7e2e7a396cba58feb4a90fb3cb63441c201ab4d3e0cd6403"
   head "https://github.com/nginx/njs.git", branch: "master"
 
   depends_on "pcre2" => :build
